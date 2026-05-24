616fb6624e7d64c5e76227efd0cd53407aac5d6a
SunnFlower47 <ridwanandrian2004@gmail.com>
2026-05-24 23:14:38 +0700

    fix : email reset password

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
 <td class="header" style="padding: 40px 0; text-align: center; background-color: #006b63;">
 <a href="{{ $url }}" style="display: inline-block; text-decoration: none;">
-<img src="{{ config('app.url') }}/assets/images/logo-desa-cibatu.png" style="height: 60px; width: auto; margin-bottom: 10px;" alt="Logo Desa Cibatu">
+<img src="{{ asset('assets/images/logo-desa-cibatu.png') }}" style="height: 60px; width: auto; margin-bottom: 10px;" alt="Logo Desa Cibatu">
 <h1 style="color: #ffffff; margin: 0; font-size: 20px; font-weight: 800; letter-spacing: -0.5px; text-transform: uppercase;">Desa Cibatu</h1>
 </a>
 </td>
